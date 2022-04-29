f3a1c5dafc9edb7c3920ac864face04321ad0180
Fenosoa Randrianjatovo <98966847+FenosoaRandrianjatovo@users.noreply.github.com>
2022-04-29 07:45:35 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -16,4 +16,3 @@ def my_function(a_f, num_epochs,use_pytorch = False):
   optimizer = torch.optim.SGD(conv_model.parameters(), lr=args.lr, momentum=args.momentum)
   train(model=conv_model, criterion=criterion, data_loader=dataloader.train_loader, optimizer=optimizer, num_epochs=num_epochs)
   test(conv_model, dataloader.test_loader,name_of_ac=a_f, use_pytorch=use_pytorch)
-
