963d3d361310b3dd1a6ccb90741f240c9ae39f3e
Fenosoa Randrianjatovo <98966847+FenosoaRandrianjatovo@users.noreply.github.com>
2022-04-29 07:29:37 +0000

    Add files via upload

--- Data/dataloader.py
+++ Data/dataloader.py
@@ -1,1 +1,21 @@
+import torch
+import torchvision
+from torchvision import datasets, transforms
 
+mnist_train = datasets.MNIST(
+    "/tmp/mnist", train=True, download=True, transform=transforms.ToTensor()
+)
+mnist_test = datasets.MNIST(
+    "/tmp/mnist", train=False, download=True, transform=transforms.ToTensor()
+)
+
+# Size of the batches the data loader will produce.
+batch_size = 64
+
+# This creates the dataloaders.
+train_loader = torch.utils.data.DataLoader(
+    mnist_train, batch_size=batch_size, shuffle=True
+)
+test_loader = torch.utils.data.DataLoader(
+    mnist_test, batch_size=batch_size, shuffle=False
+)
